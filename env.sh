04e5800a5ea26890aa161d1cb9139b7ae0f851b6
Newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-12-01 16:46:54 -0800

    fix: env.sh script

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-PAKKU_VERSION := 1.3.2
+PAKKU_VERSION=1.3.2
 
-MC_VERSION := $(shell jq -r '.mc_versions[0]' pakku-lock.json)
-FABRIC_VERSION := $(shell jq -r '.loaders.fabric' pakku-lock.json)
-FABRIC_INSTALLER_VERSION := 1.1.0
+MC_VERSION=$(jq -r '.mc_versions[0]' pakku-lock.json)
+FABRIC_VERSION=$(jq -r '.loaders.fabric' pakku-lock.json)
+FABRIC_INSTALLER_VERSION=1.1.0
 
-SERVER_NAME := $(shell jq -r .name pakku.json)
-SERVER_VERSION := $(shell jq -r .version pakku.json)
+SERVER_NAME=$(jq -r .name pakku.json)
+SERVER_VERSION=$(jq -r .version pakku.json)
