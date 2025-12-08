0b06205a6e73d03fbebe89a80338b0dafa4d19ed
Newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-12-08 15:10:18 -0800

    feat(worldgen): Reimplement Cascades with biome generation changes removed for compatibility with other worldgen mods

---
+++ datapack/cascades/cascades_source.py
@@ -0,0 +1,20 @@
+import beet
+import beet.contrib.worldgen as worldgen
+
+
+def make_cascades_source(ctx: beet.Context):
+    for namespace in ctx.data.keys():
+        namespace = ctx.data.get(namespace)
+
+        if namespace is None:
+            continue
+
+        for ty in list(namespace.keys()):
+            if ty in [
+                worldgen.WorldgenBiome,
+                worldgen.Dimension,
+                worldgen.DimensionType,
+                worldgen.WorldgenStructure,
+                beet.Advancement,
+            ]:
+                namespace.pop(ty)
