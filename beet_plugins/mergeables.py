71d37cf68d0f99e56f45d3c0e06e377d1ac447c9
Newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-12-11 20:43:59 -0800

    feat(datapack): add logging for file types that failed to merge with special handling

--- beet_plugins/mergeables.py
+++ beet_plugins/mergeables.py
@@ -12,3 +12,20 @@ def beet_default(ctx: beet.Context):
 
 worldgen.WorldgenBiome.merge = merge_biome
 worldgen.WorldgenNoise.merge = merge_noise
+
+
+def merge_with_warning(self: beet.File, other: beet.File) -> bool:
+    name = self.__class__.__name__
+
+    if name not in getattr(beet.File, "failed_merge_types") and name not in [
+        "PngFile",
+        "Advancement",
+    ]:
+        print(f"failed to merge: '{self.__class__.__name__}'")
+        getattr(beet.File, "failed_merge_types").add(name)
+
+    return False
+
+
+setattr(beet.File, "failed_merge_types", set())
+beet.File.merge = merge_with_warning
